c2aaac90372b2bf73145c791554bfd8d941c9d15
denisbaranov <dbaranov.work@gmail.com>
2019-04-02 14:21:09 +0300

    Issue: Default today's date Link: https://secure.helpscout.net/conversation/816544814/29457?folderId=646527 Done: Add filter “um_field_value”. Set current date if date value is empty.

--- includes/core/um-filters-fields.php
+++ includes/core/um-filters-fields.php
@@ -730,3 +730,23 @@ function um_edit_url_field_value( $value, $key ) {
 	return $value;
 }
 add_filter( 'um_edit_url_field_value', 'um_edit_url_field_value', 10, 2 );
+
+
+/**
+ * Set current date if date value is empty
+ * @param string	$value		"Field Value"
+ * @param string	$default	"Field Key"
+ * @param string	$key			"Field Type"
+ * @param string	$type			"Field Default Value"
+ * @param array		$data			"Field Data"
+ * @return string
+ */
+function um_field_value__default_date( $value, $default, $key, $type, $data ) {
+
+	if ( empty( $value ) && $type === 'date' ) {
+		$value = date( 'Y/m/d' );
+	}
+
+	return $value;
+}
+add_filter( 'um_field_value', 'um_field_value__default_date', 10, 5 );
